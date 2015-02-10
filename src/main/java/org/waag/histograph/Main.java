1cdea1760425259348495de1060722af6f715c59
Bert Spaan <bert.spaan@gmail.com>
2015-02-10 14:20:56 +0100

    redis!

--- src/main/java/org/waag/histograph/Main.java
+++ src/main/java/org/waag/histograph/Main.java
@@ -1,49 +1,71 @@
 package org.waag.histograph;
 
-import com.tinkerpop.gremlin.neo4j.process.graph.Neo4jGraphTraversal;
+import java.util.Iterator;
+import java.util.List;
+
+import redis.clients.jedis.Jedis;
+
+import com.tinkerpop.gremlin.driver.Client;
+import com.tinkerpop.gremlin.driver.Cluster;
+import com.tinkerpop.gremlin.driver.Result;
+import com.tinkerpop.gremlin.driver.ResultSet;
 import com.tinkerpop.gremlin.neo4j.structure.Neo4jGraph;
 import com.tinkerpop.gremlin.server.GremlinServer;
+import com.tinkerpop.gremlin.server.Graphs;
 import com.tinkerpop.gremlin.server.Settings;
-import com.tinkerpop.gremlin.structure.Vertex;
 
 public class Main {
 	
 	Neo4jGraph g;
 	GremlinServer s;
+	Jedis jedis;
 	
 	public static void main(String[] argv) {
 		new Main().start();
 	}
 
 	private void start() {
-		System.out.println("halootjes");
+		System.out.println("Starting Histograph Core");
 				
 		try {
 			Settings settings = Settings.read(getClass().getResourceAsStream("/gremlin-server-neo4j.yaml"));
-			
-			System.out.println(settings.graphs);
-			
-			s = new GremlinServer(settings);
-			
+			s = new GremlinServer(settings);			
 			s.run();
 		} catch (Exception e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		
-//		g = Neo4jGraph.open("/tmp/neo4j");
-//		Neo4jGraphTraversal<Vertex, String> hond = g.V().label();		
-//		while (hond.hasNext()) {
-//			System.out.println(hond.next());			
-//		}
-//		try {
-//			g.close();
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
+		jedis = new Jedis("localhost");
 		
+		Cluster cluster = Cluster.open();
+		Client client = cluster.connect(); 
+		
+		List<String> messages = null;
+        while(true){
+          System.out.println("Waiting for a message in the queue");
+          messages = jedis.blpop(0, "histograph-queue");
+          
+          // messages.get(1) is JSON object:
+          // {
+          //  "action": ["add", "delete", "update"],
+          //  "type": ["vertex", "edge"],
+          //  "data": {}
+          // }
+          
+          
+          System.out.println("Got the message");
+          System.out.println("KEY:" + messages.get(0) + " VALUE:" + messages.get(1));
+          String payload = messages.get(1);
+          //Do some processing with the payload
+          System.out.println("Message received:" + payload);
+          
+          // Als er is dinges dan doe iets!
+          //ResultSet results = client.submit("g.V().label()");
+        }
+				
+	
+		
+
 		
 		// 3. Start Gremlin Server
 		// 4. En blijf lekker draaien!
