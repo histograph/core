48951e1fcd5d310c4cc635465e00a2f3046ef6ca
rutgervw <rutgervanwilligen@gmail.com>
2015-03-02 17:03:41 +0100

    prepare start script for separate ES handler app (node?)

--- bin/histograph-core.sh
+++ bin/histograph-core.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ ! -d `dirname $0`/../core-main/target ] || [ ! -d `dirname $0`/../core-es/target ]; then
+if [ ! -d `dirname $0`/../core-main/target ] ; then
 	echo "Histograph Core has not been built yet! Run 'mvn clean install' first."
 	exit
 fi
 
-echo "Starting Histograph Elasticsearch controller..."
-`dirname $0`/../core-es/bin/core-es.sh $@
+# echo "Starting Histograph Elasticsearch controller..."
+# `dirname $0`/../core-es/bin/core-es.sh $@
 
-echo "Starting Histograph Main module..."
+# echo "Starting Histograph Main module..."
 `dirname $0`/../core-main/bin/core-main.sh $@
