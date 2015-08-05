cd2755b30cc97f70433b964409322b1ebfa0aa00
Bert Spaan <bert.spaan@gmail.com>
2015-08-05 14:59:49 +0200

    clean-up + code style

--- index.js
+++ index.js
@@ -1,168 +1,175 @@
-var argv = require('minimist')(process.argv.slice(2));
-
-// any string is converted to URI
-var normalizeIdentifiers = require('histograph-uri-normalizer').normalize;
-
-var H = require('highland');
-var Graphmalizer = require('graphmalizer-core');
-
 var config = require('histograph-config');
 var schemas = require('histograph-schemas');
-
+var Graphmalizer = require('graphmalizer-core');
+var H = require('highland');
 var Redis = require('redis');
-var redis_client = Redis.createClient();
-// create a stream from redis queue
-var redis = H(function redisGenerator(push, next)
-{
-    // function called on each redis message (or timeout)
-    function redisCallback(err, data) {
-        // handle error
-        if (err) {
-            push(err);
-            next();
-            return;
-        }
+var redisClient = Redis.createClient(config.redis.port, config.redis.host);
 
-        // attempt parse or error
-        try {
-            var d = JSON.parse(data[1]);
-            push(null, d);
-            next();
-        }
-        catch (e) {
-            push(e);
-            next();
-        }
+// Convert any ID, URI, URN to Histograph URN
+var normalize = require('histograph-uri-normalizer').normalize;
+
+var elasticsearch = require('elasticsearch');
+var esClient = new elasticsearch.Client({
+  host: config.elasticsearch.host + ':' + config.elasticsearch.port
+});
+
+// Create a stream from Redis queue
+var redis = H(function redisGenerator(push, next) {
+
+  // Function called on each Redis message (or timeout)
+  function redisCallback(err, data) {
+    // handle error
+    if (err) {
+      push(err);
+      next();
+      return;
     }
 
-    // blocking pull from redis
-    redis_client.blpop(config.redis.queue, 0, redisCallback);
+    // attempt parse or error
+    try {
+      var d = JSON.parse(data[1]);
+      push(null, d);
+      next();
+    }
+    catch (e) {
+      push(e);
+      next();
+    }
+  }
+
+  // blocking pull from Redis
+  redisClient.blpop(config.redis.queue, 0, redisCallback);
 });
 
 var ACTION_MAP = {
-    add: 'add',
-    update: 'add',
-    delete: 'remove'
+  add: 'add',
+  update: 'add',
+  delete: 'remove'
 };
 
-function toGraphmalizer(msg)
-{
-    function norm(x)
-    {
-        if(x)
-            return normalizeIdentifiers(x, msg.dataset);
-
-        return undefined;
+function toGraphmalizer(msg) {
+  function norm(x) {
+    if (x) {
+      return normalize(x, msg.dataset);
     }
 
-    var d = msg.data || {};
+    return undefined;
+  }
 
-    // dataset is a top-level attribute that we want copied into the 'data' attribute
-    d.dataset = msg.dataset;
+  var d = msg.data || {};
 
-    return {
-        operation: ACTION_MAP[msg.action],
+  // dataset is a top-level attribute that we want copied into the 'data' attribute
+  d.dataset = msg.dataset;
 
-        dataset: d.dataset,
+  return {
+    operation: ACTION_MAP[msg.action],
 
-        // some old data uses `label` instead of `type`
-        type: d.type,
+    dataset: d.dataset,
 
-        // nodes are identified with id's or URI's, we don't care
-        id: norm(d.id || d.uri),
+    // some old data uses `label` instead of `type`
+    type: d.type,
 
-        // formalize source/target id's
-        source: norm(d.from),
-        target: norm(d.to),
+    // nodes are identified with id's or URI's, we don't care
+    id: norm(d.id || d.uri),
 
-        data: stringifyObjectFields(d)
-    }
+    // formalize source/target id's
+    source: norm(d.from),
+    target: norm(d.to),
+
+    data: stringifyObjectFields(d)
+  };
 }
 
 // when passed an object, every field that contains an object
 // is converted into a JSON-string (*mutates in place*)
-function stringifyObjectFields(obj){
-    // convert objects to JSONified strings
-    var d = JSON.parse(JSON.stringify(obj));
-    if(typeof(d) === 'object')
-        Object.keys(d).forEach(function(k){
-            var v = d[k];
-            if(typeof(v) === 'object')
-                d[k] = JSON.stringify(v);
-        });
+function stringifyObjectFields(obj) {
+  // convert objects to JSONified strings
+  var d = JSON.parse(JSON.stringify(obj));
 
-    return d;
+  if (typeof (d) === 'object') {
+    Object.keys(d).forEach(function(k) {
+      var v = d[k];
+      if (typeof (v) === 'object') {
+        d[k] = JSON.stringify(v);
+      }
+    });
+  }
+
+  return d;
 }
 
-// index into elasticsearch
-var ES = require('elasticsearch');
-var es_client = ES.Client({host: 'localhost:9200'});
+// Index into elasticsearch
 
 // calling c.fn without 2nd argument yields a promise
-var index = es_client.index.bind(es_client);
-var remove = es_client.delete.bind(es_client);
+var index = esClient.index.bind(esClient);
+var remove = esClient.delete.bind(esClient);
 
 var OP_MAP = {
-    add: index,
-    remove: remove
+  add: index,
+  remove: remove
 };
 
 // index documents into elasticsearch
-function toElastic(data){
-    // select appropriate ES operation
-    var operation = OP_MAP[data.operation];
+function toElastic(data) {
+  // select appropriate ES operation
+  var operation = OP_MAP[data.operation];
 
-    // replace string version with original
-    try{ data.data.geometry = JSON.parse(data.data.geometry) } catch(_) {};
+  // replace string version with original
+  try {
+    data.data.geometry = JSON.parse(data.data.geometry) ;
+  } catch (_) {
+  }
 
-    var opts = {
-        index: data.dataset,
-        type: data.type,
-        id: data.id,
-        body: data.data
-    };
+  var opts = {
+    index: data.dataset,
+    type: data.type,
+    id: data.id,
+    body: data.data
+  };
 
-    // run it, returns a stream
-    return H(operation(opts));
+  // run it, returns a stream
+  return H(operation(opts));
 }
 
 function logError(err) {
-    console.error(err.stack || err);
+  console.error(err.stack || err);
 }
 
 var commands = redis
     .errors(logError)
     .map(toGraphmalizer);
+
 var neo4jAuth;
 if (config.neo4j.user && config.neo4j.password) {
   neo4jAuth = config.neo4j.user + ':' + config.neo4j.password;
 }
 
 var gconf = {
-    types: schemas.graphmalizer.types,
-    Neo4J: {
-        hostname: config.neo4j.host,
-        port: config.neo4j.port,
-        auth: neo4jAuth
-    },
-    batchSize: config.core.batchSize,
-    batchTimeout: config.core.batchTimeout
+  types: schemas.graphmalizer.types,
+  Neo4J: {
+    hostname: config.neo4j.host,
+    port: config.neo4j.port,
+    auth: neo4jAuth
+  },
+  batchSize: config.core.batchSize,
+  batchTimeout: config.core.batchTimeout
 };
 
 var graphmalizer = new Graphmalizer(gconf);
 
 graphmalizer.register(commands)
-    .map(function(d){
-        // only index nodes into elasticsearch
-        if(d.request.parameters.structure === 'node')
-            return toElastic(d.request.parameters);
+    .map(function(d) {
+      // only index nodes into elasticsearch
+      if (d.request.parameters.structure === 'node') {
+        return toElastic(d.request.parameters);
+      }
 
-        return H([]);
+      return H([]);
     })
     .series()
     .errors(logError)
-    .each(function(){
-        process.stdout.write('.');
+    .each(function() {
+      process.stdout.write('.');
     });
 
 console.log(config.logo.join('\n'));
