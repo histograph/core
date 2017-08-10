4a0ac83bc53f713d6c768ff062595e9eb5ddaf53
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2017-08-10 16:31:15 +0200

    Back-pressure regulations

--- testHighland.js
+++ testHighland.js
@@ -1,37 +1,130 @@
-const elasticsearch = require('elasticsearch');
-var esClient = new elasticsearch.Client({host: 'localhost:9200'});
+const https = require('https');
+const _=require('highland');
 
-_=require('highland');
+const Writable = require('stream').Writable;
 
 
+
+/**
+ * @param counter useless initialization
+ */
+function MyWriteStream(counter) {
+  if (!(this instanceof MyWriteStream)) {
+    return new MyWriteStream(counter);
+  }
+  Writable.call(this, {objectMode:true,highWaterMark:counter});
+  
+	// this.highWaterMark = counter
+  this.counter = 0;
+	
+	this.on('finish', function() {
+		console.log("finish called");
+  }.bind(this));
+}
+
+MyWriteStream.prototype = Object.create( Writable.prototype, {constructor: {value: MyWriteStream}} );
+
+/**
+ * @param chunk a bulk request as json.
+ */
+ 
+MyWriteStream.prototype._write = function(chunk, enc, next) {
+  
+	this.counter = this.counter + 1;
+	console.log("counter is: " + this.counter + ", chunk is: " + chunk);
+	
+  if( this.counter%9 == 0 ){
+		console.log("Returning FALSE, counter is: " + this.counter + ", chunk is: " + chunk);
+		setTimeout(this._cb,30000,this,next);
+	 	return false;
+	}
+	
+	if( this.counter%15 == 0 ){
+		next(new Error('I am a big error'));
+		return;
+	}
+	
+	
+	clFunc(chunk,next);
+	// setTimeout(next,10000,null,null);
+	
+  return true;
+
+};
+
+
+MyWriteStream.prototype._cb = function(self,next){
+	console.log("I will callback");
+	next();
+}
+
+// This does not work, empty the buffer and decrease the highwater 
+// seems to have to be done all manually
+MyWriteStream.prototype._emit = function(self,next){
+	
+	var entry = self._writableState.bufferedRequest;
+	while (entry) {
+      var chunk = entry.chunk;
+      var encoding = entry.encoding;
+      var cb = entry.callback;
+
+      self._write(chunk, encoding, cb);
+      entry = entry.next;
+  }
+	console.log("I will emit");
+	self.emit('drain');
+}
+
+MyWriteStream.prototype._final = function(callback){
+	console.log("Final called");
+	callback();
+	
+}
+
+mystream = new MyWriteStream(5);
 
 var simplePrintFunction = function(x){console.log("simplePrintFunction is: " + JSON.stringify(x));return x;};
 
-_([1,2,3,4]).map(function(x){
-	console.log("In ES is: " + x);
-	esClient.count(function (error, response, status) {
-	  if(error){
-			console.log("maledizioneee" + error);
-		}
-	  console.log("In CB is: " + x);
+var clFunc = function(x,callback){
+	console.log("In clFunc is: " + x);
+	https.get('https://api.histograph.io/search?q=burlutto', (res) => {
+	  console.log('statusCode:', res.statusCode);
+	  // console.log('headers:', res.headers);
+
+	  res.on('data', (d) => {
+			console.log("data called")
+	    // process.stdout.write(d);
+			callback();
+	  });
+
+	}).on('error', (e) => {
+		console.log("error called")
+	  console.error(e);
+	}).on('end',() => {
+		console.log("end called")
+		callback();
 	});
-	console.log("Out ES is: " + x);
-	return x;
-}).map(simplePrintFunction).done(function(){console.log("done");});
+	console.log("Out clFunc is: " + x);
+}
+
+var wrappedfunction = _.wrapCallback(clFunc);
+
+
+
+
+/*
+** 
+** FUNCTION CALLS
+**
+*/
+
+_([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]).map(simplePrintFunction).pipe(mystream);null
+
+
 
 // _([1,2,3,4]).sequence().map(simplePrintFunction).done(function(){console.log("done");});
 
-var ESfunction = function(x,callback){
-	console.log("In ES is: " + x);
-	esClient.info(callback);
-	console.log("Out ES is: " + x);
-};
 
-var wrappedfunction = _.wrapCallback(ESfunction);
-
-
-
-// wrappedfunction =  _.wrapCallback(esClient.info.bind(esClient));
 
 _([1,2,3,4]).map(simplePrintFunction).map(wrappedfunction).sequence().map(simplePrintFunction).done(function(x){console.log("done");});
 
@@ -44,3 +137,8 @@ _([1,2,3,4]).map(wrappedfunction).flatten().each(simplePrintFunction).done(funct
 _([1,2,3,4]).map(wrappedfunction).done(function(x){console.log("done");});
 
 _([1,2,3,4]).map(wrappedfunction).parallel(2).toArray(simplePrintFunction).done(function(){console.log("done");});
+
+_([1,2,3,4]).map(wrappedfunction).parallel(2).toArray(simplePrintFunction).done(function(){console.log("done");});
+
+
+
--- writableBulk.js
+++ writableBulk.js
@@ -46,6 +46,11 @@ WritableBulkIndexing.prototype._write = function(chunk, enc, next) {
 
 };
 
+WritableBulkIndexing.prototype._resetNeedsToWait = function (self,next,err,resp){
+  self.needsToWait = false;
+  next(err,resp);
+}
+
 WritableBulkIndexing.prototype._doBulkIndexing = function (chunk, enc, next){
   
   my_log.debug("Data to index is: " + JSON.stringify(chunk));
@@ -60,39 +65,44 @@ WritableBulkIndexing.prototype._doBulkIndexing = function (chunk, enc, next){
       if (/Request Timeout after/.test(err)){
         my_log.warn("Timeout, message: " + err + " response: " + JSON.stringify(resp || '{"message":"no response provided"}'));
         self.needsToWait = true;
-        setTimeout(self._doBulkIndexing,config.elasticsearch.retryTime,chunk, enc, next);
       }else{
         my_log.error("Error processing ES commands, message: " + err + " response: " + JSON.stringify(resp || '{"message":"no response provided"}'));
         my_log.error(err.stack || "No Stack Info");
-        self.emit('error', err);
+        // return the error
+        next(err);
+        return;
       }
     }else{
+      my_log.debug("ES resp: " + JSON.stringify(resp));
+      
       var r = resp || {took: 0, errors:false, items: []};
       if( r.items == null){
         r.items = [];
       }
-      my_log.debug("ES resp: " + JSON.stringify(resp));
       
       if ( r.errors ){
         var newrequest = [];
-        for (var es_error=0;es_error<r.items.length;es_error++){  
-          if (r.items[es_error].index && r.items[es_error].index.error != null ){
-            if ('es_rejected_execution_exception' == r.items[es_error].index.error.type ){
-                my_log.error("ES Queue full:" + JSON.stringify(r.items[es_error].index));  
-                // self.needsToWait = true;
+        for (var err_cnt=0;err_cnt<r.items.length;err_cnt++){  
+          if (r.items[err_cnt].index && r.items[err_cnt].index.error != null ){
+            if ('es_rejected_execution_exception' == r.items[err_cnt].index.error.type ){
+                my_log.error("ES Queue full:" + JSON.stringify(r.items[err_cnt].index));  
+                self.needsToWait = true;
+                // we should resubmit failed requests, but it is probably better to try again with the dataset
+                // and take care that the system does not get overloaded
                 // newrequest.push();
             }else{
-                my_log.error("ES response:" + JSON.stringify(r.items[es_error].index));  
+                my_log.error("ES response:" + JSON.stringify(r.items[err_cnt].index));  
             }
           }
         }
       }
-      my_log.info("ES => " + r.items.length + " indexed, took " + r.took + "ms, errors: " + r.errors);
-      if( self.needsToWait ){
-          self.needsToWait = false;
-          self.omit('drain');
-      }
-      next(err,resp);      
+      my_log.info("ES => " + r.items.length + " indexed, took " + r.took + "ms, errors: " + r.errors);  
     }
+    if( self.needsToWait ){
+      // Delay calling the callback to give some rest to the system
+      setTimeout(self._resetNeedsToWait,config.elasticsearch.retryTime,self,next,err,resp);
+    }else{
+        next(err,resp);
+    }    
   });
 }
